0c3dcb42b7aa375892db6ee0f36a687b2831e90e
Nico Flaig <nflaig@protonmail.com>
2024-08-09 11:24:25 +0100

    Fix trimming of graffiti

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -3,7 +3,7 @@
 # Handle Graffiti Character Limit
 oLang=$LANG oLcAll=$LC_ALL
 LANG=C LC_ALL=C 
-graffitiString=${GRAFFITI:0:32}
+graffitiString=$(echo "$GRAFFITI" | cut -c 1-32)
 LANG=$oLang LC_ALL=$oLcAll
 
 exec node /usr/app/node_modules/.bin/lodestar \
