3428bb882273aea9165de895afb1ed1e8af62106
Nico Flaig <nflaig@protonmail.com>
2024-08-14 18:13:58 +0100

    Double quote to prevent globbing and word splitting

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -8,7 +8,7 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS in
   HTTP_ENGINE="http://nethermind-xdai.dappnode:8551"
   ;;
 "gnosis-erigon.dnp.dappnode.eth")
-    HTTP_ENGINE="http://gnosis-erigon.dappnode:8551"
+  HTTP_ENGINE="http://gnosis-erigon.dappnode:8551"
   ;;
 *)
   echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS"
@@ -17,23 +17,22 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS in
   ;;
 esac
 
-
-exec node --max-old-space-size=${MEMORY_LIMIT} /usr/app/node_modules/.bin/lodestar \
-    beacon \
-    --network=gnosis \
-    --suggestedFeeRecipient=${FEE_RECIPIENT_ADDRESS} \
-    --jwt-secret=/jwtsecret \
-    --execution.urls=$HTTP_ENGINE \
-    --dataDir=/var/lib/data \
-    --rest \
-    --rest.address 0.0.0.0 \
-    --rest.port ${BEACON_API_PORT} \
-    --port $P2P_PORT \
-    --metrics \
-    --metrics.port 8008 \
-    --metrics.address 0.0.0.0 \
-    --logFile /var/lib/data/beacon.log \
-    --logLevel=${DEBUG_LEVEL} \
-    --logFileLevel=debug \
-    --logFileDailyRotate 5 \
-    $EXTRA_OPTS
+exec node --max-old-space-size="${MEMORY_LIMIT}" /usr/app/node_modules/.bin/lodestar \
+  beacon \
+  --network=gnosis \
+  --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
+  --jwt-secret=/jwtsecret \
+  --execution.urls="${HTTP_ENGINE}" \
+  --dataDir=/var/lib/data \
+  --rest \
+  --rest.address 0.0.0.0 \
+  --rest.port "${BEACON_API_PORT}" \
+  --port "${P2P_PORT}" \
+  --metrics \
+  --metrics.port 8008 \
+  --metrics.address 0.0.0.0 \
+  --logFile /var/lib/data/beacon.log \
+  --logLevel="${DEBUG_LEVEL}" \
+  --logFileLevel=debug \
+  --logFileDailyRotate 5 \
+  "${EXTRA_OPTS}"
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -2,14 +2,14 @@
 
 # Handle Graffiti Character Limit
 oLang=$LANG oLcAll=$LC_ALL
-LANG=C LC_ALL=C 
+LANG=C LC_ALL=C
 graffitiString=$(echo "$GRAFFITI" | cut -c 1-32)
 LANG=$oLang LC_ALL=$oLcAll
 
 exec node /usr/app/node_modules/.bin/lodestar \
     validator \
     --network=gnosis \
-    --suggestedFeeRecipient=${FEE_RECIPIENT_ADDRESS} \
+    --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
     --graffiti="${graffitiString}" \
     --dataDir=/var/lib/data \
     --keymanager true \
@@ -19,12 +19,12 @@ exec node /usr/app/node_modules/.bin/lodestar \
     --metrics \
     --metrics.port 5064 \
     --metrics.address 0.0.0.0 \
-    --externalSigner.url=${HTTP_WEB3SIGNER} \
-    --doppelgangerProtection=${DOPPELGANGER_PROTECTION} \
-    --beaconNodes=${BEACON_NODE_ADDR} \
+    --externalSigner.url="${HTTP_WEB3SIGNER}" \
+    --doppelgangerProtection="${DOPPELGANGER_PROTECTION}" \
+    --beaconNodes="${BEACON_NODE_ADDR}" \
     --http.requestWireFormat=ssz \
-    --logLevel=${DEBUG_LEVEL} \
+    --logLevel="${DEBUG_LEVEL}" \
     --logFileLevel=debug \
     --logFileDailyRotate 5 \
     --logFile /var/lib/data/validator.log \
-    $EXTRA_OPTS
+    "${EXTRA_OPTS}"
